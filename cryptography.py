8cd2ffc789ed14b57fbdceabf2bb586fb7708cd2
KraweDad <mattfenner35@gmail.com>
2016-10-14 13:39:39 -0400

    Update cryptography.py

--- cryptography.py
+++ cryptography.py
@@ -15,6 +15,10 @@ associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .
 a = []
 b = []
 f = []
+u = []
+z = []
+w = []
+
 Coding = True
 while Coding:
     start = input("Enter e to encrypt, d to decrypt, or q to quit:")
@@ -31,17 +35,42 @@ while Coding:
             p = b*20    
             for l in range(len(m)):
                 q = a[l] + p[l] #adding key's #'s to message #'s
-                f.append(q)
+                if q >= 84:
+                    o = q-84
+                    f.append(o)
+                else:
+                    f.append(q)
+            
             for h in f:
                 wow = associations[h] #Converting letters back into #'s
                 print(wow, end="") #Printing result
             print()
         elif start == "d":
-            print("Matt! focus on encrypting!")
+            m = input("Message: ")
+            for c in m:
+                n  = (associations.find(c))
+                w.append(n) #converting message to #'s
+            k = input("Key: ")
+            for c in k:
+                j = (associations.find(c))
+                z.append(j) #converting key to #'s
+            p = z*20    
+            for l in range(len(m)):
+                q = w[l] - p[l] #adding key's #'s to message #'s
+                if q < 0:
+                    o = q + 84
+                    u.append(o)
+                else:
+                    u.append(q)
+            
+            for h in u:
+                wow = associations[h] #Converting letters back into #'s
+                print(wow, end="") #Printing result
+            print()
         else:
-            print("Matt its e not " + start + " you idiot")
+            print("Did not understand command, try again.")
     else:
-        print("YOU'RE NOT ALLOWED TO QUIT YET")
+        print("Goodbye!")
         Coding = False
     
     
